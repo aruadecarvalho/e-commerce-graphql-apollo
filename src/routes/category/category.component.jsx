dcfcfa90a4702c80ba049e6187105a80b7f964e5
aruadecarvalho <aruadecarvalho@gmail.com>
2023-01-12 11:28:37 -0300

    GET_CATEGORY query for category

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,6 +1,6 @@
-import { useContext, useState, useEffect } from "react";
-import { CategoriesContext } from "../../contexts/categories.context";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
 import {
   CategoryBody,
   CategoryContainer,
@@ -8,29 +8,64 @@ import {
 } from "./category.styles.jsx";
 import ProductCard from "../../components/product-card/product-card.component";
 import { ComponentAnimation } from "../../components/animations/animations.component";
+import Spinner from "../../components/spinner/spinner.component";
+
+const GET_CATEGORY = gql`
+  query ($title: String!) {
+    getCollectionsByTitle(title: $title) {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
+
+  const { loading, error, data } = useQuery(GET_CATEGORY, {
+    variables: {
+      title: category,
+    },
+  });
+
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+    if (data) {
+      const {
+        getCollectionsByTitle: { items },
+      } = data;
+
+      setProducts(items);
+    }
+  }, [category, data]);
 
   return (
-    <ComponentAnimation>
-      <CategoryContainer>
-        <CategoryTitle>
-          {category.charAt(0).toUpperCase() + category.slice(1)}
-        </CategoryTitle>
-        <CategoryBody>
-          {products &&
-            products.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-        </CategoryBody>
-      </CategoryContainer>
-    </ComponentAnimation>
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <ComponentAnimation>
+          <CategoryContainer>
+            <CategoryTitle>
+              {category.charAt(0).toUpperCase() + category.slice(1)}
+            </CategoryTitle>
+            <CategoryBody>
+              {products &&
+                products.map((product) => (
+                  <ProductCard key={product.id} product={product} />
+                ))}
+            </CategoryBody>
+          </CategoryContainer>
+        </ComponentAnimation>
+      )}
+    </>
   );
 };
 
